42cb2f9ecea7dde87cea94155e3e0d6545152f09
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-04-03 17:35:42 -0500

    clase de oscar 8

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path'); // Para trabajar con archivos y rutas de directorios
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production', // le pasamos explicitamente el modo desde el archivo
+    entry: './src/index.js', // el punto de entrada de mi aplicación
+    output: { // Esta es la salida de mi bundle
+        path: path.resolve(__dirname, 'public_html/js'),
+        // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
+        // para no tener conflictos entre Linux, Windows, etc
+        filename: 'main.js', 
+        // EL NOMBRE DEL ARCHIVO FINAL,
+    },
+    resolve: {
+        extensions: ['.js'] // LOS ARCHIVOS QUE WEBPACK VA A LEER
+    },
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader
+                test: /\.m?js$/,
+                // Exclude permite omitir archivos o carpetas especificas
+                exclude: /node_modules/,
+                // Use es un arreglo u objeto donde dices que loader aplicaras
+                use: {
+                loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+    
+}
